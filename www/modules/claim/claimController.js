9d7542d04120a8edc91ece36a16091d34dceb499
jyoti <jyotichettri@lftechnology.com>
2015-02-07 11:04:12 +0545

    cleans claim restaurant and adds verify restaurant

--- www/modules/claim/claimController.js
+++ www/modules/claim/claimController.js
@@ -5,14 +5,9 @@ rxControllers.controller('claimCtrl', ['$scope', '$modal', '$routeParams', '$tim
 
     function ($scope, $modal, $routeParams, $timeout, messageCenterService, claimSvr, localStorageService) {
 
-        $scope.claimForm = {};
-        $scope.claimForm.restaurant_id = $routeParams.restaurantId;
-        $scope.percent = 0;
-        $scope.max = 5;
-        var modalInstance = null;
-
         var user = localStorageService.get('user');
-        $scope.claimForm.user_id = user.id;
+        initForm();
+        var modalInstance = null;
 
         $scope.claimRestaurant = function () {
 
@@ -28,7 +23,25 @@ rxControllers.controller('claimCtrl', ['$scope', '$modal', '$routeParams', '$tim
                     messageCenterService.add('success', 'Your access code has been sent to your email.', {timeout : 3000});
                     $timeout(closeModel, 3000);
                 }else{
-                    clearForm ();
+                    initForm ();
+                }
+            });
+        }
+
+        $scope.verifyRestaurant = function () {
+
+            claimSvr.verifyRestaurant($scope.claimForm).then(function (response) {
+                if(response.err){
+                    response.data.forEach(function(item){
+                        messageCenterService.add('danger', item.message, {timeout : 3000});
+                    });
+                    return;
+                }
+
+                if(response.status < 400){
+                    //@TODO success
+                }else{
+                    initForm ();
                 }
             });
         }
@@ -36,37 +49,13 @@ rxControllers.controller('claimCtrl', ['$scope', '$modal', '$routeParams', '$tim
         function closeModel (){
             modalInstance.close('');
             $(".overlay-main").css("display", "none");
-            clearForm ();
+            initForm ();
         }
 
-        function clearForm (){
+        function initForm (){
             $scope.claimForm = {};
             $scope.claimForm.restaurant_id = $routeParams.restaurantId;
             $scope.claimForm.user_id = user.id;
-        }
-
-
-
-        $scope.claim = function(){
-
-            modalInstance = $modal.open({
-                templateUrl: "modules/claim/views/form.html",
-                scope: $scope,
-                windowClass: "claim-modal-window",
-                controller: "claimCtrl"
-            });
-
-            modalInstance.opened.then(function () {
-                $scope.showModal = true;
-                $(".overlay-main").css("display", "block");
-            });
-
-            modalInstance.result.then(function (selectedItem) {
-                $scope.selected = selectedItem;
-            }, function () {
-                $(".overlay-main").css("display", "none");
-            });
-
         }
 
     }]);
