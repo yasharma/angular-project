76f74a312c086f1211f49877bbaf8ed5c4568c37
jyoti <jyotichettri@lftechnology.com>
2015-02-07 11:27:30 +0545

    minor : changes icon

--- www/modules/home/indexController.js
+++ www/modules/home/indexController.js
@@ -295,7 +295,7 @@ rxControllers.config(['$routeProvider', function ($routeProvider) {
                     {
                         name : 'Request Change',
                         link : '#/request/change/' + $scope.restaurantId,
-                        icon : 'i-statistics'
+                        icon : 'i-pencil'
                     }
                 ];
             }
